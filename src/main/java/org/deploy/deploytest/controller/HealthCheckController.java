3fef2f1e2e3ae8b77a5b845526ce3e796011a514
younssue <love_rvd7@naver.com>
2024-02-18 00:54:30 +0900

    :art: deploy test

---
+++ src/main/java/org/deploy/deploytest/controller/HealthCheckController.java
@@ -0,0 +1,39 @@
+package org.deploy.deploytest.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HealthCheckController {
+    @Value("${server.env}")
+    private String env;
+    @Value("${server.port}")
+    private String serverPort;
+    @Value("${server.address}")
+    private String address;
+    @Value("${serverName}")
+    private String serverName;
+
+
+
+    @GetMapping("/hc")
+    public ResponseEntity<?> healthCheck(){
+        Map<String,String> responseData = new HashMap<>();
+        responseData.put("serverPort", serverPort);
+        responseData.put("address", address);
+        responseData.put("serverName", serverName);
+        responseData.put("env",env);
+
+        return ResponseEntity.ok(responseData);
+    }
+
+    @GetMapping("/env")
+    public ResponseEntity<?> getEnv(){
+        return ResponseEntity.ok(env);
+    }
+}
