c26605fae44a744dd6aa11828ab5d34317ce74c7
younssue <love_rvd7@naver.com>
2024-02-24 01:05:34 +0900

    :art: Add DB Test function

--- src/main/java/org/deploy/deploytest/repository/HealthCheckRepository.java
+++
@@ -1,7 +0,0 @@
-package org.deploy.deploytest.repository;
-
-import org.deploy.deploytest.domain.HealthCheck;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HealthCheckRepository extends JpaRepository<HealthCheck,Long> {
-}
